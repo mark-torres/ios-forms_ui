6c016a8c7a59c0bf654f3bce54dc32cc17661de5
Mark Torres <mark.torres.mets@gmail.com>
2018-05-17 19:02:46 -0500

    Initial Commit

---
+++ Forms UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forms UI
+//
+//  Created by Marcos Torres on 5/17/18.
+//  Copyright © 2018 HSoft Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
